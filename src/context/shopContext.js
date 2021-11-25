89857bcc09869df8ce7358e9eb71b32d9827fa98
kmcandersen <kristen.andersen@gmail.com>
2021-11-24 20:25:34 -0600

    manage state with reducer

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import Client from 'shopify-buy';
 
 const ShopContext = React.createContext();
@@ -15,25 +15,108 @@ const initialState = {
   isCartOpen: false,
 };
 
+const shopReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_ALL_PRODUCTS': {
+      return { ...state, products: action.payload };
+    }
+    case 'FETCH_ONE_PRODUCT': {
+      return { ...state, product: action.payload };
+    }
+    case 'CREATE_CHECKOUT': {
+      return { ...state, checkout: action.payload };
+    }
+    case 'FETCH_CHECKOUT': {
+      return { ...state, checkout: action.payload };
+    }
+    case 'ADD_CHECKOUT_ITEM': {
+      return { ...state, checkout: action.payload };
+    }
+    case 'TOGGLE_CART': {
+      return { ...state, isCartOpen: action.payload };
+    }
+    default:
+      return state;
+  }
+};
+
 export const ShopProvider = ({ children }) => {
-  const [state, setState] = useState(initialState);
+  const [state, dispatch] = useReducer(shopReducer, initialState);
 
   useEffect(() => {
+    createCheckout();
+
+    const fetchAllProducts = async () => {
+      const products = await client.product.fetchAll();
+      dispatch({
+        type: 'FETCH_ALL_PRODUCTS',
+        payload: products,
+      });
+    };
     fetchAllProducts();
   }, []);
 
-  const fetchAllProducts = async () => {
-    const products = await client.product.fetchAll();
-    console.log('products', products);
-    setState({ products: products });
+  const fetchProductWithHandle = async (handle) => {
+    const product = await client.product.fetchByHandle(handle);
+    dispatch({
+      type: 'FETCH_ONE_PRODUCT',
+      payload: product,
+    });
+  };
+
+  const createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    localStorage.setItem('checkout-id', checkout.id);
+    dispatch({
+      type: 'CREATE_CHECKOUT',
+      payload: checkout,
+    });
+  };
+
+  const fetchCheckout = (checkoutId) => {
+    client.checkout
+      .fetch(checkoutId)
+      .then((checkout) => {
+        dispatch({
+          type: 'FETCH_CHECKOUT',
+          payload: checkout,
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const addItemToCheckout = async (variantId, quantity) => {
+    const lineItemsToAdd = [
+      { variantId: variantId, quantity: parseInt(quantity, 10) },
+    ];
+    const checkout = await client.checkout.addLineItems(
+      state.checkout.id,
+      lineItemsToAdd
+    );
+    dispatch({
+      type: 'ADD_CHECKOUT_ITEM',
+      payload: checkout,
+    });
+    toggleCart(true);
   };
 
   const toggleCart = (bool) => {
-    setState({ isCartOpen: bool });
+    dispatch({
+      type: 'TOGGLE_CART',
+      payload: bool,
+    });
   };
 
   return (
-    <ShopContext.Provider value={{ state, fetchAllProducts, toggleCart }}>
+    <ShopContext.Provider
+      value={{
+        state,
+        fetchProductWithHandle,
+        fetchCheckout,
+        addItemToCheckout,
+        toggleCart,
+      }}
+    >
       {children}
     </ShopContext.Provider>
   );
