5cf09c1e8c0f37c9ac310ecd9d828c96292054bb
kmcandersen <kristen.andersen@gmail.com>
2021-11-30 10:40:34 -0600

    revise OutlinedLinkButton click handler prop

--- src/components/CartEmpty.js
+++ src/components/CartEmpty.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box, Typography } from '@mui/material';
+import ShopContext from '../context/shopContext';
 import { OutlinedLinkButton } from '../components/AppButton';
 
 const styles = {
@@ -12,10 +13,14 @@ const styles = {
 };
 
 export default function CartEmpty() {
+  const { toggleCart } = useContext(ShopContext);
   return (
     <Box sx={{ ...styles.messageContainer }}>
       <Typography paragraph>Your shopping cart is empty</Typography>
-      <OutlinedLinkButton route={`/products`}>
+      <OutlinedLinkButton
+        route={`/products`}
+        clickHandler={() => toggleCart(false)}
+      >
         Start Shopping
       </OutlinedLinkButton>
     </Box>
