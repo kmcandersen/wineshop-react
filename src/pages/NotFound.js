9384a2fc79314cbc8c04721918911bc0a7db29ff
kmcandersen <kristen.andersen@gmail.com>
2021-12-10 17:50:14 -0600

    style NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
+import { Box, Container, useMediaQuery } from '@mui/material';
+import { BodyTextSpecial, PageHead } from './../components/AppText';
+import { OutlinedGoHomeButton } from './../components/AppButton';
+import customTheme from '../styles/theme';
+
+const styles = {
+  containerXS: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'left',
+  },
+  container: {
+    textAlign: 'left',
+  },
+};
 
 export default function NotFound() {
-  const location = useLocation();
-  const message = location.state ? location.state.message : null;
+  // could be used to render more specific error messages:
+  // const location = useLocation();
+  // const message = location.state ? location.state.message : null;
+
+  const xsScreen = useMediaQuery('(max-width:400px)');
 
   document.title = 'Page Not Found';
 
   return (
-    <div>
-      <h1>NOT FOUND</h1>
-      <h3>{message}</h3>
-    </div>
+    <Container
+      style={xsScreen ? { ...styles.containerXS } : { ...styles.container }}
+    >
+      <PageHead>Page not found</PageHead>
+      <BodyTextSpecial>We can't find what you're looking for.</BodyTextSpecial>
+      <Box sx={{ mt: customTheme.spacing(9) }}>
+        <OutlinedGoHomeButton />
+      </Box>
+    </Container>
   );
 }
